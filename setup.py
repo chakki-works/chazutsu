d754a74aa5d78796e8f61bf1d1b22d93fd40a414
icoxfog417 <icoxfog417@yahoo.co.jp>
2017-05-12 16:13:18 +0900

    add setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name = "chazutsu",
+    version = "0.1",
+    description = "The tool to make NLP datasets ready to use",
+    keywords = ["machine learning", "nlp", "natural language processing"],
+    author = "icoxfog417",
+    author_email = "icoxfog417@yahoo.co.jp",
+    license="Apache License 2.0",
+    packages = [
+        "chazutsu",
+        "chazutsu.datasets",
+        "chazutsu.datasets.framework"
+        ],
+    url = "https://github.com/chakki-works/chazutsu",
+    download_url = "https://github.com/chakki-works/chazutsu/archive/0.1.tar.gz",
+    install_requires=[
+        "rarfile>=3.0",
+        "requests>=2.14.2",
+        "tqdm>=4.11.2"
+    ],
+)
