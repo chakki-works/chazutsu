84caf858ec28aefd756c0f280a096f892e3d6611
icoxfog417 <icoxfog417@yahoo.co.jp>
2017-05-15 16:29:05 +0900

    update to 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "chazutsu",
-    version = "0.2",
+    version = "0.3",
     description = "The tool to make NLP datasets ready to use",
     keywords = ["machine learning", "nlp", "natural language processing"],
     author = "icoxfog417",
@@ -15,7 +15,7 @@ setup(
         "chazutsu.datasets.framework"
         ],
     url = "https://github.com/chakki-works/chazutsu",
-    download_url = "https://github.com/chakki-works/chazutsu/archive/0.2.tar.gz",
+    download_url = "https://github.com/chakki-works/chazutsu/archive/0.3.tar.gz",
     install_requires=[
         "rarfile>=3.0",
         "requests>=2.14.2",
